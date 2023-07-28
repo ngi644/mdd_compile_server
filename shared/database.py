0521b5b5f40bd898f01a3d0b9270042e699bab8e
Takashi Nagai <ngi644@gmail.com>
2023-07-28 13:12:30 +0900

    bug fix

--- shared/database.py
+++ shared/database.py
@@ -1,11 +1,11 @@
 # database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 DATABASE_URL = "postgresql://user:password@db:5432/compile_results"
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base()
